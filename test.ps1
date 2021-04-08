6a41abeb6bac40b854378c2fe47d2fb9e9d05be6
Manohar C <manoharc07@gmail.com>
2021-04-09 01:01:55 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,7 +1,7 @@
 write-host $env:pass
 $SrvPassword = ConvertTo-SecureString "$($ENV:pass)" -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential ("jenkins@manoharc19.onmicrosoft.com", $SrvPassword)
-connect-azuread -credential $cred
+connect-azaccount -credential $cred
 
 $SecureStringPassword = ConvertTo-SecureString -String "Asdf1234@#" -AsPlainText -Force
 New-AzADUser -DisplayName "MyDisplayName" -UserPrincipalName "myemail@manoharc19.onmicrosoft.com" -Password $SecureStringPassword -MailNickname "MyMailNickName"
