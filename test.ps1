811fc9bc27ff87b247ce1d316f299dfe879caf52
Manohar C <manoharc07@gmail.com>
2021-04-09 01:08:37 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,4 +1,4 @@
-write-host $env:pass
+write-host $($env:pass)
 $SrvPassword = ConvertTo-SecureString "$($ENV:pass)" -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential ("jenkins@manoharc19.onmicrosoft.com", $SrvPassword)
 connect-azaccount -credential $cred
