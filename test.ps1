3c9ff54e967c62adc4a1142ef1eea3db05fb3dc7
Manohar C <manoharc07@gmail.com>
2021-04-09 00:50:27 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,5 +1,5 @@
 write-host $env:pass
 $SrvPassword = ConvertTo-SecureString "$($ENV:pass)" -AsPlainText -Force
-$cred = New-Object System.Management.Automation.PSCredential ("automation@manoharc19.onmicrosoft.com", $SrvPassword)
+$cred = New-Object System.Management.Automation.PSCredential ("jenkins@manoharc19.onmicrosoft.com", $SrvPassword)
 connect-azuread -credential $cred
 
