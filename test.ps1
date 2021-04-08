ab2b55b3ff7dc2b144bac45322c9c5342de608ee
Manohar C <manoharc07@gmail.com>
2021-04-08 16:05:12 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,4 +1,7 @@
-$SrvPassword = ConvertTo-SecureString "$($ENV:pass)" -AsPlainText -Force
-$cred = New-Object System.Management.Automation.PSCredential ("$ENV:username", $SrvPassword)
-connect-azuread -credential $cred
-get-azureaduser
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$true)]
+    [string] $Name
+)
+write-host $Name
+    
