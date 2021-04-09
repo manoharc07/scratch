ed3f1b8c490ed0c3f0e52aee6aa3580b53023f7e
Manohar C <manoharc07@gmail.com>
2021-04-09 14:43:25 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,4 +1,8 @@
-write-host $($env:pass)
+[cmdletBindings]
+param(
+[string]$name
+)
+write-host $named awd
 $SrvPassword = ConvertTo-SecureString "$($ENV:pass)" -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential ("automation@manoharc19.onmicrosoft.com",$srvPassword)
 connect-azaccount -Credential $cred
