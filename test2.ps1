4e4573cd03ec641cbea81245b1cd89ca9ce596fc
Manohar C <manoharc07@gmail.com>
2021-04-09 14:44:18 +0530

    Update test2.ps1

--- test2.ps1
+++ test2.ps1
@@ -1,7 +1,11 @@
+[cmdletBindings]
+param(
+[string]$name
+)
 write-host $($env:pass)
 $SrvPassword = ConvertTo-SecureString "$($ENV:pass)" -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential ("automation@manoharc19.onmicrosoft.com",$srvPassword)
 connect-azaccount -Credential $cred
 
 $SecureStringPassword = ConvertTo-SecureString -String "Asdf1234@#" -AsPlainText -Force
-New-AzADUser -DisplayName "MyDisplayName" -UserPrincipalName "email@manoharc19.onmicrosoft.com" -Password $SecureStringPassword -MailNickname "MyMailNickName"
+New-AzADUser -DisplayName "MyDisplayName" -UserPrincipalName "myel@manoharc19.onmicrosoft.com" -Password $SecureStringPassword -MailNickname "MyMailNickName"
